c3e7f99373fab60338d6d009ed86a6df73a53cc2
Gabriel Peres Magni <gpmagni@protonmail.com>
2023-04-03 08:23:31 -0300

    New patterns - WIP

--- app.py
+++ app.py
@@ -12,32 +12,18 @@ from pdfminer.converter import PDFPageAggregator
 import pandas as pd
 import openpyxl
 
-class PDFManager:
-    def __init__(self, folder):
-        self.files = []
-        self.nome_pasta_onde_salvar = 'final'
-        self.folder = folder
-        self.table_file = self.get_table_file()
-        self.list_folder_files(self.folder)
-        self.get_file_data()
-        self.generate_table()
+class OriginTypeInterface:
+    def handle_file(files, doNext):
+        pass
 
-
-    def generate_table(self):
-        self.table_file.to_excel('Relação de Notas x Cidades.xlsx')
-
-
-    def get_table_file(self):
-        return pd.DataFrame({'Número da nota': [], 'Cidade': []})
-    
-    
-    def get_file_data(self):
-        nf_locator = 'Número:\n\n'
-        city_locator = 'Local da Prestação do Serviço:'
-        city_locator_1 = 'Endereço Obra:'
-        city_locator_2 = 'Natureza da Operação:'
-        for index, file in enumerate(self.files):
-            print(f'{index+1}/{len(self.files)} - {(index/len(self.files)) * 100}%')
+class OriginType2(OriginTypeInterface):
+    def handle_file(files, doNext):
+        nf_locator = 'Número da\n\nNFS-e\n\n'
+        nf_locator1 = 'Nº '
+        city_locator = 'Dados do Tomador de Serviços'
+        city_locator1 = 'MUNÍCIPIO\n\n'
+        for index, file in enumerate(files):
+            print(f'{index+1}/{len(files)} - {(index/len(files)) * 100}%')
             file_binary = open(file, 'rb')
             parser = PDFParser(file_binary)
             doc = PDFDocument(parser)
@@ -52,7 +38,76 @@ class PDFManager:
                 interpreter.process_page(page)
                 layout = device.get_result()
                 layout.analyze(laparams)
-                pdf_text = layout.groups[0].get_text()
+                pdf_text: str = layout.groups[0].get_text()
+
+                try:
+                    if pdf_text.find(nf_locator) >= 0:
+                        nf_position = pdf_text.find(nf_locator) + len(nf_locator)
+                    else:
+                        nf_position = pdf_text.find(nf_locator1) + len(nf_locator1)
+                    nf_content = pdf_text[nf_position:pdf_text.find('\n', nf_position)].strip()
+                    
+                    if pdf_text.find(city_locator) >= 0:
+                        city_position = (pdf_text.find(city_locator) + len(city_locator))
+                        city_position = re.search(r'Município\n\n', pdf_text[city_position:-1]).end() + city_position
+                    else:
+                        city_position = (pdf_text.find(city_locator1) + len(city_locator1))
+                except AttributeError:
+                    print('DEBUG')
+                    print(file)
+                    print(city_content)
+
+                    print('--------CONTEÚDO--------')
+                    print(pdf_text)
+
+                    print('FIM DEBUG')
+                    exit()
+
+                city_content = pdf_text[city_position:pdf_text.find('\n', city_position + 2)].strip()
+                try:
+                    try:
+                        city_content = re.search(r'^(.+?[\-])', city_content).group(0)
+                    except AttributeError:
+                        pass
+                except AttributeError:
+                    print('DEBUG')
+                    print(file)
+                    print(city_content)
+
+                    print('--------CONTEÚDO--------')
+                    print(pdf_text)
+
+                    print('FIM DEBUG')
+                    exit()
+
+                city_content = re.sub(r'[^\w ]', '', city_content).capitalize()
+                city_content = city_content.strip()
+                doNext(nf_content, city_content, file)
+
+
+class OriginType1(OriginTypeInterface):
+    def handle_file(files, doNext):
+        nf_locator = 'Número:\n\n'
+        city_locator = 'Local da Prestação do Serviço:'
+        city_locator_1 = 'Endereço Obra:'
+        city_locator_2 = 'Natureza da Operação:'
+        for index, file in enumerate(files):
+            print(f'{index+1}/{len(files)} - {(index/len(files)) * 100}%')
+            file_binary = open(file, 'rb')
+            parser = PDFParser(file_binary)
+            doc = PDFDocument(parser)
+            laparams = LAParams()
+            if not doc.is_extractable:
+                print('PDFTextExtractionNotAllowed')
+            rsrcmgr = PDFResourceManager()
+            device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+            interpreter = PDFPageInterpreter(rsrcmgr, device)
+            for index, page in enumerate(PDFPage.create_pages(doc)):
+                if index > 0: break
+                interpreter.process_page(page)
+                layout = device.get_result()
+                layout.analyze(laparams)
+                pdf_text: str = layout.groups[0].get_text()
 
                 nf_position = pdf_text.find(nf_locator) + len(nf_locator)
                 nf_content = pdf_text[nf_position:pdf_text.find('\n', nf_position)].strip()
@@ -81,10 +136,36 @@ class PDFManager:
                     break
 
                 city_content = re.sub(r'[^\w ]', '', city_content).capitalize()
-                self.organize_files(nf_content, city_content, file)
+                doNext(nf_content, city_content, file)
+
+class PDFManager:
+    def __init__(self, folder, origin_type: OriginTypeInterface):
+        self.set_origin_type(origin_type)
+        self.files = []
+        self.nome_pasta_onde_salvar = 'final'
+        self.folder = folder
+        self.table_file = self.get_table_file()
+        self.list_folder_files(self.folder)
+        self.get_file_data()
+        self.generate_table()
 
 
-    def organize_files(self, nf_number, nf_city, file):
+    def set_origin_type(self, origin_type: OriginTypeInterface):
+        self.origin_type = origin_type
+
+    def generate_table(self):
+        self.table_file.to_excel('Relação de Notas x Cidades.xlsx')
+
+
+    def get_table_file(self):
+        return pd.DataFrame({'Número da nota': [], 'Cidade': []})
+    
+    
+    def get_file_data(self):
+        self.origin_type.handle_file(self.files, self.organize_files)
+
+
+    def organize_files(self, nf_number: str, nf_city: str, file):
             pasta_completa_para_salvar = f'{self.nome_pasta_onde_salvar}{path.sep}{nf_city}'
             new_row = pd.DataFrame({'Número da nota': [nf_number], 'Cidade': [nf_city]}, columns=self.table_file.columns)
             self.table_file = pd.concat([self.table_file, new_row], ignore_index=True)
@@ -94,12 +175,12 @@ class PDFManager:
                 pass
 
             try:
-                system(f'cp {file} "{pasta_completa_para_salvar}{path.sep}{nf_number}.pdf"')
+                system(f'cp "{file}" "{pasta_completa_para_salvar}{path.sep}{nf_number}.pdf"')
             except FileNotFoundError:
                 print('Erro, arquivo não encontrado')
     
 
-    def list_folder_files(self, dir, debug=True):
+    def list_folder_files(self, dir, debug=False):
         if debug:
             print('Listando diretórios...')
         try:
@@ -126,4 +207,4 @@ class PDFManager:
                 print('Erro não tratado list_folder_files: '+str(e))
             
 
-app = PDFManager('arquivos')
+PDFManager('arquivos', OriginType2)
